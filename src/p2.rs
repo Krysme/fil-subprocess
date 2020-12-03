164f7ddd8f0bd0aeb6dc8e945299fe027f86aa06
yangdonglin <332407470@qq.com>
2020-12-03 18:13:20 +0800

    add log for p2 subprocess

--- src/p2.rs
+++ src/p2.rs
@@ -2,10 +2,12 @@ use filecoin_proofs::process;
 use log::info;
 fn main() {
     env_logger::init();
+    info!("subprocess started");
     std::panic::set_hook(Box::new(|_| {
         let bt = backtrace::Backtrace::new();
         info!("panic occured, backtrace: {:?}", bt);
     }));
+
     if let Err(e) = process::p2_sub_launcher() {
         info!("{:?}", e);
         std::process::exit(255);
